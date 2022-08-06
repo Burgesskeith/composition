4265bcf76766bcbd5ba57d4947d4034fd00631f6
Burgesskeith <keith.burgess@webwings.com.au>
2022-08-06 20:29:22 +1000

    Tidy up

--- src/components/Tickets.js
+++ src/components/Tickets.js
@@ -3,8 +3,11 @@ import React from "react";
 const Tickets = ({ children }) => {
   return (
     <>
-      <div className="mx-auto text-center my-4 text-3xl">
+      <div className="mx-auto text-center my-4 text-3xl mb-6">
         The Main Tickets Page
+      </div>
+      <div className="ml-7 mb-6">
+        We're placing all tickets in the list component below...
       </div>
       <div>
         <hr />
